1e02a20f938abab54836910128200537542779a1
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-10-17 09:36:29 -0300

    SingletonEager

--- src/one/digitalinnovation/gof/SingletonEager.java
+++ src/one/digitalinnovation/gof/SingletonEager.java
@@ -1,4 +1,14 @@
 package one.digitalinnovation.gof;
 
 public class SingletonEager {
+
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstance() {
+        return instancia;
+    }
 }
