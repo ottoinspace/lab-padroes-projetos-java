d07e7c0f60e2443bf0c767ad1eac018cf39c9acf
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-10-17 10:17:19 -0300

    Facade

---
+++ src/subsistema2/cep/CepApi.java
@@ -0,0 +1,20 @@
+package subsistema2.cep;
+
+public class CepApi {
+    private static CepApi instancia = new CepApi();
+
+    private CepApi() {
+        super();
+    }
+    public static CepApi getInstance(){
+        return instancia;
+    }
+
+    public String recuperarCidade(String cep) {
+        return "Brasilia";
+    }
+
+    public String recuperarEstado(String cep){
+        return "DF";
+    }
+}
